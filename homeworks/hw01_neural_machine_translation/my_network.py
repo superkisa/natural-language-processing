ce1adb090c9a91e29d8c375704dd74dd50a19628
fromelicks <>
2023-11-08 19:57:16 +0100

    done 28

--- homeworks/hw01_neural_machine_translation/my_network.py
+++ homeworks/hw01_neural_machine_translation/my_network.py
@@ -1,132 +1,186 @@
 import torch
 import torch.nn as nn
-
-
+from icecream import ic
 import random
+
+ic.configureOutput(includeContext=True)
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+    def __init__(self, input_dim, emb_dim, enc_hid_dim, dec_hid_dim, n_layers, dropout):
         super().__init__()
 
-        self.input_dim = input_dim
-        self.emb_dim = emb_dim
-        self.hid_dim = hid_dim
-        self.n_layers = n_layers
-
         self.embedding = nn.Embedding(num_embeddings=input_dim, embedding_dim=emb_dim)
-        self.dropout = nn.Dropout(p=dropout)  # <YOUR CODE HERE>
+        self.dropout = nn.Dropout(p=dropout)
         self.rnn = nn.LSTM(
             input_size=emb_dim,
-            hidden_size=hid_dim,
+            hidden_size=enc_hid_dim,
             num_layers=n_layers,
             dropout=dropout,
+            bidirectional=True,
         )
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
 
     def forward(self, src):
-        # ? src = [src sent len, batch size]
+        # ? src =      [src sent len, batch size]
 
         embedded = self.embedding(src)
         embedded = self.dropout(embedded)
         # ? embedded = [src sent len, batch size, emb dim]
 
         output, (hidden, cell) = self.rnn(embedded)
-        # ? outputs = [src sent len, batch size, hid dim * n directions]
-        # ? hidden = [n layers * n directions, batch size, hid dim]
-        # ? cell = [n layers * n directions, batch size, hid dim]
+        # ? outputs =  [src sent len, batch size, hid dim * 2]
+        # ? hidden =   [n layers * 2, batch size, hid dim]
+        # ? cell =     [n layers * 2, batch size, hid dim]
 
-        # outputs are always from the top hidden layer
+        # hidden is stacked [forward_1, backward_1, forward_2, backward_2, ...]
+        # outputs are always from the last layer
 
-        return output, hidden, cell
+        # hidden [-2, :, : ] is the last of the forwards RNN
+        # hidden [-1, :, : ] is the last of the backwards RNN
+
+        hidden = torch.tanh(
+            self.fc(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
+        )
+        cell = torch.tanh(self.fc(torch.cat((cell[-2, :, :], cell[-1, :, :]), dim=1)))
+        # ? outputs =  [src len, batch size, enc hid dim * n_directions]
+        # ? hidden =   [batch size, dec hid dim]
+        # ? cell =   [batch size, dec hid dim]
+
+        return output, hidden.unsqueeze(0), cell.unsqueeze(0)
 
 
 class Attention(nn.Module):
-    def __init__(self, encoder_hid_dim, decoder_hid_dim):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
         super().__init__()
-        self.attn = nn.Linear(encoder_hid_dim, decoder_hid_dim)
-        self.softmax = nn.Softmax(dim=0)
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias=False)
 
     def forward(self, hidden, encoder_outputs):
-        encoder_outputs = self.attn(encoder_outputs)
+        # ? hidden =          [batch size, dec hid dim]
+        # ? encoder_outputs = [src len, batch size, enc hid dim * 2]
 
-        # ? hidden: [batch, dec_hid_dim]
-        # ? encoder_outputs_{q,b,d} * hidden_{d,b} = scores_{q,b}
-        scores = torch.einsum("qbd,bd->qb", encoder_outputs, hidden[0])
-        # Calculates pairwise dot product of `hidden` and each query element for batch
-        scores = self.softmax(scores)
-        return scores  # [query, batch]
+        # repeat decoder hidden state src_len times
+        hidden = hidden.squeeze(0).unsqueeze(1).repeat(1, encoder_outputs.shape[0], 1)
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        # ? hidden =          [batch size, src len, dec hid dim]
+        # ? encoder_outputs = [batch size, src len, enc hid dim * 2]
+
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+        # ? energy =          [batch size, src len, dec hid dim]
+
+        attention = self.v(energy).squeeze(2)
+        # ? attention =       [batch size, src len]
+
+        return nn.functional.softmax(attention, dim=1)
 
 
 class Decoder(nn.Module):
-    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout):
         super().__init__()
 
-        self.emb_dim = emb_dim
-        self.hid_dim = hid_dim
         self.output_dim = output_dim
-        self.n_layers = n_layers
-        self.dropout_p = dropout
 
+        self.attention = Attention(enc_hid_dim, dec_hid_dim)
         self.embedding = nn.Embedding(num_embeddings=output_dim, embedding_dim=emb_dim)
-        self.attention = Attention(hid_dim, hid_dim)
         self.rnn = nn.LSTM(
-            input_size=emb_dim,
-            hidden_size=hid_dim,
-            num_layers=n_layers,
-            dropout=dropout,
+            input_size=(enc_hid_dim * 2) + emb_dim,
+            hidden_size=dec_hid_dim,
         )
 
-        self.out = nn.Linear(
-            in_features=self.hid_dim * self.n_layers + self.hid_dim + self.emb_dim,
-            out_features=output_dim,
-        )
+        self.fc_out = nn.Linear((enc_hid_dim * 2) + dec_hid_dim + emb_dim, output_dim)
 
         self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self, input, hidden, cell, encoder_outputs):
-        scores = self.attention(hidden, encoder_outputs)
-        # ? encoder_outputs = [query, batch, enc_hid_dim]
-        # ? scores          = [query, batch]
-        # ? hidden          = [n layers * n directions, batch size, hid dim]
-        # ? cell            = [n layers * n directions, batch size, hid dim]
+    def forward(self, input_, hidden, cell, encoder_outputs):
+        # ? input =           [batch size]
+        # ? hidden =          [batch size, dec hid dim]
+        # ? cell =            [batch size, dec hid dim]
+        # ? encoder_outputs = [src len, batch size, enc hid dim * 2]
 
-        w_t = torch.einsum("qbe,qb->be", encoder_outputs, scores)
-        # ? w_t             = [batch, query]
-        w_t = w_t.unsqueeze(0)
+        input_ = input_.unsqueeze(0)
+        # ? input =           [1, batch size]
 
-        # n directions in the decoder will both always be 1, therefore:
-        # ? hidden          = [n layers, batch size, hid dim]
-        # ? context         = [n layers, batch size, hid dim]
+        embedded = self.embedding(input_)
+        embedded = self.dropout(embedded)
+        # ? embedded =        [1, batch size, emb dim]
 
-        input = self.embedding(input.unsqueeze(0))
+        a = self.attention(hidden, encoder_outputs)
+        # ? a =               [batch size, src len]
+        a = a.unsqueeze(1)
+        # ? a =               [batch size, 1, src len]
 
-        w_t = w_t.squeeze(0)
-        input = input.squeeze(0)
-        hidden_flattened = hidden.permute(1, 2, 0).reshape(hidden.shape[1], -1)
-        # [n_layers, n_batches, hid_dims] -> [n_batches, hid_dims * n_layers]
-        # Flattens along layers and passes hidden state of each layer to self.out
-        output = self.out(torch.cat((input, hidden_flattened, w_t), dim=-1))
-        return output, hidden, cell
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        # ? encoder_outputs = [batch size, src len, enc hid dim * 2]
+
+        weighted = torch.bmm(a, encoder_outputs)
+        # ? weighted =        [batch size, 1, enc hid dim * 2]
+        weighted = weighted.permute(1, 0, 2)
+        # ? weighted =        [1, batch size, enc hid dim * 2]
+
+        rnn_input = torch.cat((embedded, weighted), dim=2)
+        # ? rnn_input =        [1, batch size, (enc hid dim * 2) + emb dim]
+
+        output, (hidden_dec, cell_dec) = self.rnn(rnn_input, (hidden, cell))
+        # ? output = [seq len, batch size, dec hid dim * 1]
+        # ? hidden = [n layers * 1, batch size, dec hid dim]
+        # ? cell =   [n layers * 1, batch size, dec hid dim]
+
+        # seq len, n layers and n directions will always be 1 in this decoder, therefore:
+        # ? output = [1, batch size, dec hid dim]
+        # ? hidden = [1, batch size, dec hid dim]
+        # ? cell =   [1, batch size, dec hid dim]
+        # this also means that output == hidden
+
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+
+        prediction = self.fc_out(torch.cat((output, weighted, embedded), dim=1))
+        # ? prediction = [batch size, output dim]
+
+        return prediction, hidden_dec, cell_dec
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self, encoder: Encoder, decoder: Decoder, device):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        enc_emb_dim,
+        dec_emb_dim,
+        enc_hid_dim,
+        dec_hid_dim,
+        enc_n_layers=2,
+        enc_dropout=0.5,
+        dec_dropout=0.5,
+        device=None,
+    ):
         super().__init__()
 
-        self.encoder = encoder
-        self.decoder = decoder
-        self.device = device
-
-        assert (
-            encoder.hid_dim == decoder.hid_dim
-        ), "Hidden dimensions of encoder and decoder must be equal!"
-        assert (
-            encoder.n_layers == decoder.n_layers
-        ), "Encoder and decoder must have equal number of layers!"
+        self.encoder = Encoder(
+            input_dim=input_dim,
+            emb_dim=enc_emb_dim,
+            enc_hid_dim=enc_hid_dim,
+            dec_hid_dim=dec_hid_dim,
+            n_layers=enc_n_layers,
+            dropout=enc_dropout,
+        )
+        self.decoder = Decoder(
+            output_dim=output_dim,
+            emb_dim=dec_emb_dim,
+            enc_hid_dim=enc_hid_dim,
+            dec_hid_dim=dec_hid_dim,
+            dropout=dec_dropout,
+        )
+        if device is None:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        else:
+            self.device = device
 
     def forward(self, src, trg, teacher_forcing_ratio=0.5):
-        # src = [src sent len, batch size]
-        # trg = [trg sent len, batch size]
+        # ? src = [src sent len, batch size]
+        # ? trg = [trg sent len, batch size]
         # teacher_forcing_ratio is probability to use teacher forcing
         # e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
 
@@ -142,13 +196,13 @@ class Seq2Seq(nn.Module):
         encoder_outputs, hidden, cell = self.encoder(src)
 
         # first input to the decoder is the <sos> tokens
-        input = trg[0, :]
+        input_ = trg[0, :]
 
         for t in range(1, max_len):
-            output, hidden, cell = self.decoder(input, hidden, cell, encoder_outputs)
+            output, hidden, cell = self.decoder(input_, hidden, cell, encoder_outputs)
             outputs[t] = output
             teacher_force = random.random() < teacher_forcing_ratio
             top1 = output.max(1)[1]
-            input = trg[t] if teacher_force else top1
+            input_ = trg[t] if teacher_force else top1
 
         return outputs
